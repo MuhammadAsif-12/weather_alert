f3e29809f411bd4d464f835e75848f53996f3523
Muhammad Asif <m.asif.addi@gmail.com>
2024-12-05 12:09:36 +0500

    added repository

--- lib/features/home_page/domain/repository/weather_bylatlong_repo.dart
+++ lib/features/home_page/domain/repository/weather_bylatlong_repo.dart
@@ -2,5 +2,5 @@ import 'package:weather_home_screen/core/resource/data_state.dart';
 import 'package:weather_home_screen/features/home_page/domain/entities/entity_weather_data.dart';
 
 abstract class WeatherBylatlongRepo {
-  Future<DataState<EntityWeatherData>> getWeatherByLatLong();
+  Future<DataState<EntityWeatherData>> getWeatherByLatLong(Map<String, dynamic> request);
 }
