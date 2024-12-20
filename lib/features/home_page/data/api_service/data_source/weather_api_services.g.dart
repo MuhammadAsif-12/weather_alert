ea1300a18f328385c0b9e67961548c99cf1d417b
Muhammad Asif <m.asif.addi@gmail.com>
2024-12-20 18:46:43 +0500

    reslove parse error logger

--- lib/features/home_page/data/api_service/data_source/weather_api_services.g.dart
+++ lib/features/home_page/data/api_service/data_source/weather_api_services.g.dart
@@ -90,3 +90,12 @@ class _WeatherApiService implements WeatherApiService {
     return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
+/// Base class for logging errors that occur during parsing of response data.
+abstract class ParseErrorLogger {
+  /// Logs an error that occurred during parsing of response data.
+  ///
+  /// - [error] is the error that occurred.
+  /// - [stackTrace] is the stack trace of the error.
+  /// - [options] are the options that were used to make the request.
+  void logError(Object error, StackTrace stackTrace, RequestOptions options);
+}
